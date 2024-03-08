72fe05a2e6aaa4a7a40e32b2523513ce9ce2e634
Kyryh <kyrykiller07@gmail.com>
2024-03-08 08:26:59 +0100

    Removed unused import

--- Hooks.cs
+++ Hooks.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using static RoR2.CharacterAI.BaseAI;
 
 namespace PermaPings {
     internal static class Hooks {
