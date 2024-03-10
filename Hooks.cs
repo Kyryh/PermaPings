ba46c332ef4d896301195acdd171166a3c9d54d8
Kyryh <kyrykiller07@gmail.com>
2024-03-10 23:21:01 +0100

    Update Hooks.cs

--- Hooks.cs
+++ Hooks.cs
@@ -1,4 +1,5 @@
 ﻿using RoR2;
+using RoR2.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,13 +13,13 @@ namespace PermaPings {
             On.RoR2.UI.PingIndicator.Update += PingIndicator_Update;
         }
         
-        private static void PingIndicator_Update(On.RoR2.UI.PingIndicator.orig_Update orig, RoR2.UI.PingIndicator self) {
+        private static void PingIndicator_Update(On.RoR2.UI.PingIndicator.orig_Update orig, PingIndicator self) {
             if (!PermaPingerController.permapingsIndicators.Contains(self)) {
                 orig(self);
             }
         }
 
-        private static void PlayerCharacterMasterController_Update(On.RoR2.PlayerCharacterMasterController.orig_Update orig, RoR2.PlayerCharacterMasterController self) {
+        private static void PlayerCharacterMasterController_Update(On.RoR2.PlayerCharacterMasterController.orig_Update orig, PlayerCharacterMasterController self) {
             orig(self);
             if (self.hasEffectiveAuthority && self.bodyInputs && self.body && Input.GetKeyDown(KeyCode.Mouse3))
                 PermaPingerController.AttemptPing(new Ray(self.bodyInputs.aimOrigin, self.bodyInputs.aimDirection), self.body.gameObject, self.gameObject);
